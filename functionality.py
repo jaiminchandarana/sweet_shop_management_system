d982c9ccd40407103832cd3cb8e0efdf9cbb5885
jaiminchandarana <137302744+jaiminchandarana@users.noreply.github.com>
2025-07-17 14:22:52 +0530

    Implement show sweet feature

--- functionality.py
+++ functionality.py
@@ -21,3 +21,14 @@ def delete(name):
         conn.close()
     except Exception as e:
         print("This is product is not in stock.")
+        
+def show():
+    conn = connection()
+    cur = conn.cursor()
+    cur.execute("SELECT name,category,price,quantity FROM incubyte")
+    rows = cur.fetchall()
+    for row in rows:
+        return row 
+    conn.commit()
+    cur.close()
+    conn.close()
